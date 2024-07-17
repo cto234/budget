38afb2cf2f537638c2749fc864e6919c18b0ebc0
cto234 <cto234@nyu.edu>
2024-07-17 15:02:16 -0400

    Fixed Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import FinanceManager from './components/FinanceManager/FinanceManager';
 
 function App() {
   return (
     <>
-      <FinanceManager />
+    <Router>
+      <Routes>
+        <Route path='/' element={<FinanceManager />} />
+      </Routes>
+    </Router>
     </>
   );
 }
